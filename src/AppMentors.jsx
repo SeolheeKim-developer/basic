fbdd85a708b156e5d7d624c805a150bb74d772e7
SeolheeKim-developer <seolhee@campigotto.com>
2023-02-25 19:33:39 -0500

    improve states

--- src/AppMentors.jsx
+++ src/AppMentors.jsx
@@ -1,20 +1,47 @@
 import React, { useState } from "react";
 
 export default function AppMentors() {
-  const [person, setPerson] = useState({
-    name: "seolhee",
-    title: "Developer",
-    mentors: [
-      {
-        name: "Ellie",
-        title: "Senior Developer",
-      },
-      {
-        name: "Gabriel",
-        title: "Senior Developer",
-      },
-    ],
-  });
+  const [person, setPerson] = useState(initialPerson);
+  const handleUpdate = () => {
+    const prev = prompt(`which mentor's name needs to be changed?`);
+    const current = prompt(`What is the name?`);
+    /*setPerson((prevPerson) => {
+      const updatedMentors = prevPerson.mentors.map((mentor) => {
+        if (mentor.name === prev) {
+          return { ...mentor, name: current };
+        }
+        return mentor;
+      });
+
+      return { ...prevPerson, mentors: updatedMentors };
+    });*/
+    setPerson((person) => ({
+      ...person,
+      mentors: person.mentors.map((mentor) => {
+        if (mentor.name === prev) {
+          return { ...mentor, name: current };
+        }
+        return mentor;
+      }),
+    }));
+  };
+  const handleAdd = () => {
+    const addName = prompt(`Enter a new mentor's name`);
+    const addTitle = prompt(`Enter a new mentor's Title`);
+    const newMentor = { name: addName, title: addTitle };
+    const updatedPerson = {
+      ...person,
+      mentors: [...person.mentors, newMentor],
+    };
+    setPerson(updatedPerson);
+  };
+  const handleDelete = () => {
+    const remove = prompt(`Enter the mentor name for deleting?`);
+    setPerson((person) => ({
+      ...person,
+      mentors: person.mentors.filter((mentor) => mentor.name !== remove),
+    }));
+  };
   return (
     <div>
       <h1>
@@ -28,58 +55,24 @@ export default function AppMentors() {
           </li>
         ))}
       </ul>
-      <button
-        onClick={() => {
-          const prev = prompt(`which mentor's name needs to be changed?`);
-          const current = prompt(`What is the name?`);
-          /*setPerson((prevPerson) => {
-            const updatedMentors = prevPerson.mentors.map((mentor) => {
-              if (mentor.name === prev) {
-                return { ...mentor, name: current };
-              }
-              return mentor;
-            });
-
-            return { ...prevPerson, mentors: updatedMentors };
-          });*/
-          setPerson((person) => ({
-            ...person,
-            mentors: person.mentors.map((mentor) => {
-              if (mentor.name === prev) {
-                return { ...mentor, name: current };
-              }
-              return mentor;
-            }),
-          }));
-        }}
-      >
-        Change the name of mentor
-      </button>
-      <button
-        onClick={() => {
-          const addName = prompt(`Enter a new mentor's name`);
-          const addTitle = prompt(`Enter a new mentor's Title`);
-          const newMentor = { name: addName, title: addTitle };
-          const updatedPerson = {
-            ...person,
-            mentors: [...person.mentors, newMentor],
-          };
-          setPerson(updatedPerson);
-        }}
-      >
-        Add a mentor
-      </button>
-      <button
-        onClick={() => {
-          const remove = prompt(`Enter the mentor name for deleting?`);
-          setPerson((person) => ({
-            ...person,
-            mentors: person.mentors.filter((mentor) => mentor.name !== remove),
-          }));
-        }}
-      >
-        Delete a mentor
-      </button>
+      <button onClick={handleUpdate}>Change the name of mentor</button>
+      <button onClick={handleAdd}>Add a mentor</button>
+      <button onClick={handleDelete}>Delete a mentor</button>
     </div>
   );
 }
+
+const initialPerson = {
+  name: "seolhee",
+  title: "Developer",
+  mentors: [
+    {
+      name: "Ellie",
+      title: "Senior Developer",
+    },
+    {
+      name: "Gabriel",
+      title: "Senior Developer",
+    },
+  ],
+};
